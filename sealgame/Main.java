854d127b5da58b5ea1a133df3f8af6c71d51bb39
timstr <f2a0b@ugrad.cs.ubc.ca>
2017-10-08 11:52:27 -0700

    Display test image

---
+++ sealgame/Main.java
@@ -0,0 +1,25 @@
+package sealgame;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hey Woild");
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
+
+        frame.add(new TestImage());
+
+        frame.pack();
+
+        frame.setVisible(true);
+    }
+
+}
